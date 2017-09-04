c3391e1cef55e1365e3662414891ca5c4c12c798
allyson.manoel <amnv@cin.ufpe.br>
2017-09-04 17:27:44 -0300

    Some improvements in managing css from wicket

--- site/src/main/java/br/itep/page/TesteAtributos.java
+++ site/src/main/java/br/itep/page/TesteAtributos.java
@@ -1,18 +1,19 @@
 package br.itep.page;
 
 import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
 public class TesteAtributos extends WebPage {
 	public TesteAtributos() {
-		Label a = new Label("a", "primeiro");
-		a.add(new SimpleAttributeModifier("class", "outro"));
+		WebMarkupContainer a = new WebMarkupContainer("a");
+		a.add(new WebMarkupContainer("as"));
+		a.get("as").add(new SimpleAttributeModifier("id", "a"));
 		add(a);
 		Label b = new Label("b", "segundo");
 		b.add(new SimpleAttributeModifier("id", "a"));
 		add(b);
-		
 		
 		add(new Label("c", "terceiro"));
 	}
